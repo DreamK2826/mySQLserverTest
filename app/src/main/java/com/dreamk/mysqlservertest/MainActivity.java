fe56a1574cc04b4ca791b4e3aeb4d08711c8504f
DreamK2826 <jurui2826@qq.com>
2024-05-28 21:53:22 +0800

    暂存一下

--- app/src/main/java/com/dreamk/mysqlservertest/MainActivity.java
+++ app/src/main/java/com/dreamk/mysqlservertest/MainActivity.java
@@ -1,6 +1,9 @@
 package com.dreamk.mysqlservertest;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button[] buttons = {findViewById(R.id.btn_test_connection),findViewById(R.id.btn_register),
+            findViewById(R.id.btn_login),findViewById(R.id.btn_check)};
+    EditText[] editTexts = {findViewById(R.id.et_sqlAddress),findViewById(R.id.et_sqlPort),
+            findViewById(R.id.et_userName),findViewById(R.id.et_password),
+            findViewById(R.id.et_id_check)};
+    TextView[] textViews = {findViewById(R.id.tv_username1),findViewById(R.id.tv_isDel1)};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
     }
+
+
 }
---
+++ app/src/main/java/com/dreamk/mysqlservertest/utils/JDBCUtils.java
@@ -0,0 +1,36 @@
+package com.dreamk.mysqlservertest.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * function： 数据库工具类，连接数据库用
+ */
+public class JDBCUtils {
+    private static final String TAG = "JDBCUtils";
+    private static final String driver = "org.mariadb.jdbc.Driver";// Sql驱动
+
+
+    /**
+     * 获取 Connection
+     *
+     * @param user     SQL user
+     * @param password SQL user password
+     * @param dbName   数据库名称
+     * @param ip       地址
+     * @param port     端口
+     * @return 获取到的 connection
+     */
+    public static Connection getConnection(String user, String password, String dbName, String ip, int port) {
+        Connection connection = null;
+        try {
+            Class.forName(driver);// 动态加载类
+            // 尝试建立到给定数据库URL的连接
+            connection = DriverManager.getConnection(
+                    "jdbc:mariadb://" + ip + ":" + port + "/" + dbName, user, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
