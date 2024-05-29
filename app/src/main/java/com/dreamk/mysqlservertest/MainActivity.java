3a41e765010670a87a5eab44f6843cfafd4cc823
DreamK2826 <jurui2826@qq.com>
2024-05-29 23:07:02 +0800

    20240529_暂存

--- app/src/main/java/com/dreamk/mysqlservertest/MainActivity.java
+++ app/src/main/java/com/dreamk/mysqlservertest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.dreamk.mysqlservertest;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +13,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import com.dreamk.mysqlservertest.utils.JDBCUtils;
+import com.dreamk.mysqlservertest.utils.ToastUtil;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -25,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     EditText et_sqlAddress,et_sqlPort,et_userName,et_password,et_id_check;
 
     TextView tv_username1,tv_isDel1;
+    SharedPreferences sp;
 
 
     @Override
@@ -38,8 +42,22 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        findView();
-        setListener();
+        new Thread(() -> {
+            findView();
+            setListener();
+            sp = getSharedPreferences("addrCfg1", Context.MODE_PRIVATE);
+            if (sp.getBoolean("isSaved", false)) {
+                et_sqlPort.setText(sp.getString("port", ""));
+                et_sqlAddress.setText(sp.getString("addr", ""));
+            }
+//         else {
+//            SharedPreferences.Editor editor = sp.edit();
+//            editor.putString("addr", et_sqlAddress.getText().toString());
+//            editor.putString("port", et_sqlPort.getText().toString());
+//            editor.putBoolean("isSaved", true);
+//            editor.apply();
+//        }
+        }).start();
 
     }
 
@@ -59,9 +77,43 @@ public class MainActivity extends AppCompatActivity {
         tv_isDel1 = findViewById(R.id.tv_isDel1);
     }
 
+    private void setSP() {
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString("addr", et_sqlAddress.getText().toString());
+        editor.putString("port", et_sqlPort.getText().toString());
+        editor.putBoolean("isSaved", true);
+        editor.apply();
+    }
+
     private void setListener(){
+        btn_test_connection.setOnClickListener(v -> {
+            setSP();
+            new Thread(() -> {
+                String sqlUser = "userAndroid", sqlPassword = "2826DreamK", dbName = "mytest1";
+                String ip_et = et_sqlAddress.getText().toString();
+                String port_et_str = et_sqlPort.getText().toString();
+                int port_et = 3306;
+                if (!port_et_str.isEmpty()) {
+                    port_et = Integer.parseInt(port_et_str);
+                }
+                Connection connection = JDBCUtils.getConnection(sqlUser, sqlPassword, dbName, ip_et, port_et);
+                runOnUiThread(() -> {
+                    if (connection == null) {
+                        ToastUtil.show(this, "connection is null!");
+                    } else {
+                        ToastUtil.show(this, "connection is OK!");
+                        try {
+                            connection.close();
+                        } catch (SQLException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                });
+            }).start();
+        });
 
         btn_check.setOnClickListener(v ->{
+            setSP();
             new Thread(() -> {
                 String id_et = et_id_check.getText().toString();
                 String ip_et = et_sqlAddress.getText().toString();
@@ -71,7 +123,7 @@ public class MainActivity extends AppCompatActivity {
                     port_et = Integer.parseInt(port_et_str);
                 }
                 if(id_et.isEmpty()){
-                    id_et=("1");
+                    id_et = ("-1");
                 }
                 String sqlUser = "userAndroid",sqlPassword = "2826DreamK",dbName = "mytest1";
                 ResultSet resultSet;
@@ -79,31 +131,38 @@ public class MainActivity extends AppCompatActivity {
                     Connection connection = JDBCUtils.getConnection(sqlUser,sqlPassword,dbName,ip_et, port_et);
                     String sql1 = "select * from mytest1.test1 where id = (?)";
                     if(connection != null){
-
                         PreparedStatement ps = connection.prepareStatement(sql1);
                         ps.setInt(1,Integer.parseInt(id_et));
                         resultSet = ps.executeQuery();
-
+                        boolean hasData = false;
                         while (resultSet.next()) {
-                            String a = resultSet.getString("username");
-                            String b = resultSet.getString("isDel");
+                            hasData = true;
 
+                            final String[] a = {resultSet.getString("username")};
+                            final String[] b = {resultSet.getString("isDel")};
                             runOnUiThread(() -> {
-                                tv_username1.setText(a);
-                                tv_isDel1.setText(b);
+                                tv_username1.setText(a[0]);
+                                tv_isDel1.setText(b[0]);
                             });
                         }
+                        if (!hasData) {
+                            runOnUiThread(() -> {
+                                ToastUtil.show(this, "未查询到数据！");
+                                tv_username1.setText("无数据");
+                                tv_isDel1.setText("无数据");
+                            });
+                        }
+                        resultSet.close();
+                        ps.close();
+                        connection.close();
+                    } else {
+                        runOnUiThread(() -> ToastUtil.show(this, "无法连接至mySQL！"));
                     }
                 } catch (SQLException e) {
                     e.printStackTrace();
-//                throw new RuntimeException(e);
+                    throw new RuntimeException(e);
                 }
-
             }).start();
-
         });
-
     }
-
-
 }
